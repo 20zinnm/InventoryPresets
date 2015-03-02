e02e86935d1bf0c32254e0c4d995765b4a1093c1
20zinnm <meyerzinn@gmail.com>
2015-03-01 21:15:19 -0600

    80%ish done

---
+++ src/com/gmail/meyerzinn/InventoryPresets/InventoryPresets.java
@@ -0,0 +1,116 @@
+package com.gmail.meyerzinn.InventoryPresets;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+import java.util.UUID;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class InventoryPresets extends JavaPlugin {
+
+	public static HashMap<String, HashMap<String, PlayerPreset>> presets = new HashMap<String, HashMap<String, PlayerPreset>>();
+
+	@SuppressWarnings("unchecked")
+	public static HashMap<String, HashMap<String, PlayerPreset>> load(File f) {
+		try {
+			@SuppressWarnings("resource")
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(
+					f.getAbsolutePath()));
+			Object result = ois.readObject();
+			return (HashMap<String, HashMap<String, PlayerPreset>>) result;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static void save(HashMap<String, HashMap<String, PlayerPreset>> map, String path) {
+		try {
+			ObjectOutputStream oos = new ObjectOutputStream(
+					new FileOutputStream(path));
+			oos.writeObject(map);
+			oos.flush();
+			oos.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void onEnable() {
+
+	}
+
+	public void onDisable() {
+
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label,
+			String[] args) {
+		if (!(sender instanceof Player)) {
+			sender.sendMessage(ChatColor.RED
+					+ "Whoops! You can't use that! You need to be a player :P");
+			return true;
+		}
+		if (args.length != 1) {
+			sender.sendMessage("Invalid arguments!");
+			return true;
+		}
+		Player p = (Player) sender;
+		if (cmd.getLabel().equalsIgnoreCase("savepreset")) {
+			PlayerPreset pp = new PlayerPreset();
+			pp.invItems = p.getInventory().getContents();
+			pp.invArmour = p.getInventory().getArmorContents();
+			HashMap CurrentPresets = presets.get((p.getUniqueId().toString()));
+			CurrentPresets.put(args[0], pp);
+			presets.remove(p.getUniqueId().toString());
+			presets.put(p.getUniqueId().toString(), CurrentPresets);
+			p.sendMessage(ChatColor.BLUE + "Saved preset " + args[0]);
+			return true;
+		}
+		if (cmd.getLabel().equalsIgnoreCase("loadpreset")) {
+			if (!presets.containsKey(p.getUniqueId().toString())) {
+				p.sendMessage(ChatColor.RED + "You have no saved presets!");
+				return true;
+			}
+			HashMap CurrentPresets = presets.get(p.getUniqueId().toString());
+			if (!CurrentPresets.containsKey(args[0])) {
+				p.sendMessage(ChatColor.RED
+						+ "There are no presets saved to you by that name!");
+				return true;
+			}
+			PlayerPreset preset = (PlayerPreset) CurrentPresets.get(args[0]);
+			p.getInventory().setContents((ItemStack[]) preset.invItems);
+			p.getInventory().setArmorContents((ItemStack[]) preset.invArmour);
+			p.sendMessage(ChatColor.BLUE + "Equipped preset! Enjoy.");
+			return true;
+		}
+		if (cmd.getLabel().equalsIgnoreCase("deletepreset")) {
+			if (!presets.containsKey(p.getUniqueId().toString())) {
+				p.sendMessage(ChatColor.RED + "You have no saved presets!");
+				return true;
+			}
+			HashMap CurrentPresets = presets.get(p.getUniqueId().toString());
+			if (!CurrentPresets.containsKey(args[0])) {
+				p.sendMessage(ChatColor.RED
+						+ "There are no presets saved to you by that name!");
+				return true;
+			}
+			CurrentPresets.remove(args[0]);
+			presets.remove(p.getUniqueId().toString());
+			presets.put(p.getUniqueId().toString(), CurrentPresets);
+			return true;
+		}
+		return false;
+
+	}
+
+}
---
+++ src/com/gmail/meyerzinn/InventoryPresets/PlayerPreset.java
@@ -0,0 +1,18 @@
+package com.gmail.meyerzinn.InventoryPresets;
+
+import java.util.UUID;
+
+import org.bukkit.inventory.ItemStack;
+
+public class PlayerPreset {
+	
+	public Integer presetNumber;
+	public ItemStack[] invItems;
+	public ItemStack[] invArmour;
+	public UUID uuid;
+
+	public PlayerPreset() {
+		
+	}
+	
+}
